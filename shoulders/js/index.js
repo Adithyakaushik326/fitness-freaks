74c68a80f6af8d44980d9b2de5a510fd048dc69b
Adithyakaushik326 <53081975+Adithyakaushik326@users.noreply.github.com>
2019-08-18 20:35:42 +0530

    Add files via upload

---
+++ shoulders/js/index.js
@@ -0,0 +1,20 @@
+function fmy()
+   { 
+       var sec=30;
+      var cn = setInterval(function(){
+           sec--;     
+          document.getElementById("p1").innerHTML= "00:"+ sec; 
+          if(sec<=0)      
+          {
+                            alert("TIMEOUT");
+                            clearInterval(cn);
+                 }
+                    },1000);}
+function fn()
+{
+    
+    document.getElementById("p1").innerHTML="00:30"
+}
+function goback(){
+    window.location.href = "../index.html";
+}
